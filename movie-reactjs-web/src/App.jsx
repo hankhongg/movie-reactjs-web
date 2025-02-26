92981def9df233adfd356f41c736d241029b54a4
Khong <hankhongg@gmail.com>
2025-02-26 14:02:10 +0700

    fetching movie from api

--- movie-reactjs-web/src/App.jsx
+++ movie-reactjs-web/src/App.jsx
@@ -1,9 +1,45 @@
 import React from "react";
 import Search from "./components/Search";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+// API - Application Programming Interface - a set of rules that allows one software application to talk to another
+
+const BASE_API_KEY= 'https://api.themoviedb.org/3';
+const API_KEY= import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
+
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const fetchMovies = async () => {
+    try {
+      const endPoint=`${BASE_API_KEY}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endPoint, API_OPTIONS);
+
+      if (!response.ok){
+        throw new Error('Something went wrong while fetching data');
+      }
+      const data = await response.json();
+      if (data.respone === false){
+        setErrorMessage(data.Error || 'Something went wrong while fetching data');
+      }
+    } catch (error) {
+      console.log({error});
+      setErrorMessage('Something went wrong. Please try again later.');
+    }
+  };
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   return (
     <div className="wrapper">
@@ -12,10 +48,15 @@ const App = () => {
         <h1>
           Find <span className="text-gradient">Movies</span> that you'd love here without efforts!
         </h1>
-        
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
       </header>
-      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-      <h1>{searchTerm}</h1>
+      
+      <section>
+        <h2 className='all-movies'>
+          All Movies
+        </h2>
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+      </section>
     </div>
   )
 }
