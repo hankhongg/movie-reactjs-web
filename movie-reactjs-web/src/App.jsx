f2acc71f3c2185096a0c724ee008f0b03550f8c9
Khong <hankhongg@gmail.com>
2025-02-26 12:53:58 +0700

    initiating the header

--- movie-reactjs-web/src/App.jsx
+++ movie-reactjs-web/src/App.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import Search from "./components/Search";
+import { useState } from "react";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="wrapper">
+      <header>
+        <img className="w-100 h-100" src="./movies-banner.jpg" alt="Movies Banner"/>
+        <h1>
+          Find <span className="text-gradient">Movies</span> that you'd love here without efforts!
+        </h1>
+        
+      </header>
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
     </div>
   )
 }
